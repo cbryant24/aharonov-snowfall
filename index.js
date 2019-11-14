bfd9f8a23a707724ca0c0b0850f0f98fc93e0a14
Chris <chrs.bryant@gmail.com>
2019-11-14 10:17:50 -0800

    first commit

--- index.js
+++ index.js
@@ -1,105 +1,19 @@
 $(document).ready(function () {
 
-  $('.article-highlight__description.zero').css({ height: '100%', display: 'block', opacity: 1});
-  
-  $('.article-highlight__images-box').on('click', function () {
-    var $this = $(this),
-      $highlightZero           = $('.article-highlight__description.zero'),
-      $highlightOne           = $('.article-highlight__description.one'),
-      $highlightTwo           = $('.article-highlight__description.two'),
-      $highlightThree         = $('.article-highlight__description.three'),
-      $highlightZeroVisible    = $('.article-highlight__description.zero:visible').length,
-      $highlightOneVisible    = $('.article-highlight__description.one:visible').length,
-      $highlightTwoVisible    = $('.article-highlight__description.two:visible').length,
-      $highlightThreeVisible  = $('.article-highlight__description.three:visible').length,
-      $highlightImageOne      = $('.article-highlight__images-box.one img'),
-      $highlightImageTwo      = $('.article-highlight__images-box.two img'),
-      $highlightImageThree    = $('.article-highlight__images-box.three img');
+  $.fn.isInViewport = function () {
+    var elementTop = $(this).offset().top;
+    var elementBottom = elementTop + $(this).outerHeight();
+    var viewportTop = $(window).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+    return elementBottom > viewportTop && elementTop < viewportBottom;
+  };
 
-    if ($this.hasClass('one') && $highlightOneVisible) {
-      $highlightOne.hide().css({ height: '0%', opacity: 0 });
-      $highlightImageOne.css('filter', 'grayscale(100%)');
-      $('.article-highlight__description.zero').css({ height: '100%', display: 'block', opacity: 1});
-      return
-
-      
-    };
-
-    if ($this.hasClass('two') && $highlightTwoVisible) {
-      $highlightTwo.hide().css({ height: '0%', opacity: 0 });
-      $highlightImageTwo.css('filter', 'grayscale(100%)');
-      $('.article-highlight__description.zero').css({ height: '100%', display: 'block', opacity: 1});
-      return
-    };
-
-    if ($this.hasClass('three') && $highlightThreeVisible) {
-      $highlightThree.hide().css({ height: '0%', opacity: 0 });
-      $highlightImageThree.css('filter', 'grayscale(100%)');
-      $('.article-highlight__description.zero').css({ height: '100%', display: 'block', opacity: 1});
-      return
-    };
-
-    if ($this.hasClass('one')) {
-      if ($highlightTwoVisible) {
-        $highlightTwo.hide().css({ height: '0%', opacity: 0 });
-        $highlightImageTwo.css('filter', 'grayscale(100%)');
+  $(window).on('resize scroll', function() {
+    console.log('hello world') 
+    $('.color-red').each( function() {
+      if ($(this).isInViewport()) {
+        $(this).addClass('letter-color');
       }
-
-      if ($highlightThreeVisible) {
-        $highlightThree.hide().css({ height: '0%', opacity: 0 });
-        $highlightImageThree.css('filter', 'grayscale(100%)');
-      }
-
-      if($highlightZeroVisible) {
-        $('.article-highlight__description.zero').hide().css({ height: '0%', opacity: 0});
-      }
-
-      if (!$highlightOneVisible) {
-        $highlightOne.show().css({ height: '100%',opacity: 1 });
-        $highlightImageOne.css('filter', 'grayscale(0%)');
-      }
-    }
-
-    if ($this.hasClass('two')) {
-      if ($highlightOneVisible) {
-        $highlightOne.hide().css({ height: '0%', opacity: 0 });
-        $highlightImageOne.css('filter', 'grayscale(100%)');
-      }
-
-      if ($highlightThreeVisible) {
-        $highlightThree.hide().css({ height: '0%', opacity: 0 });
-        $highlightImageThree.css('filter', 'grayscale(100%)');
-      }
-
-      if($highlightZeroVisible) {
-        $('.article-highlight__description.zero').hide().css({ height: '0%', opacity: 0});
-      }
-
-      if (!$highlightTwoVisible) {
-        $highlightTwo.show().css({ height: '100%',opacity: 1 });
-        $highlightImageTwo.css('filter', 'grayscale(0%)');
-      }
-    }
-
-    if ($this.hasClass('three')) {
-      if ($highlightTwoVisible) {
-        $highlightTwo.hide().css({ height: '0%', opacity: 0 });
-        $highlightImageTwo.css('filter', 'grayscale(100%)');
-      }
-
-      if ($highlightOneVisible) {
-        $highlightOne.hide().css({ height: '0%', opacity: 0 });
-        $highlightImageOne.css('filter', 'grayscale(100%)');
-      }
-
-      if($highlightZeroVisible) {
-        $('.article-highlight__description.zero').hide().css({ height: '0%', opacity: 0});
-      }
-
-      if (!$highlightThreeVisible) {
-        $highlightThree.show().css({ height: '100%',opacity: 1 });
-        $highlightImageThree.css('filter', 'grayscale(0%)');
-      }
-    }
-  })
+    }) 
+  });
 })
